6f4717aa39b17c382f4cf835c2541900e379fac1
Ashley Vick <ashleyvick@github.com>
2021-02-13 15:14:30 -0700

    Completed all sections. all green in spec.

--- nesting.js
+++ nesting.js
@@ -11,35 +11,35 @@
 // Do not edit the code below.
 var employees = [
   {
-    "firstName": "Von",
-    "lastName": "Budibent",
-    "email": "vbudibent0@163.com",
-    "department": "Sales"
+    firstName: "Von",
+    lastName: "Budibent",
+    email: "vbudibent0@163.com",
+    department: "Sales",
   },
   {
-    "firstName": "Catherina",
-    "lastName": "Swalowe",
-    "email": "cswalowe1@example.com",
-    "department": "Engineering"
+    firstName: "Catherina",
+    lastName: "Swalowe",
+    email: "cswalowe1@example.com",
+    department: "Engineering",
   },
   {
-    "firstName": "Theo",
-    "lastName": "Trill",
-    "email": "ttrill2@sina.com.cn",
-    "department": "Services"
+    firstName: "Theo",
+    lastName: "Trill",
+    email: "ttrill2@sina.com.cn",
+    department: "Services",
   },
   {
-    "firstName": "Elsy",
-    "lastName": "McCrorie",
-    "email": "emccrorie3@netscape.com",
-    "department": "Legal"
+    firstName: "Elsy",
+    lastName: "McCrorie",
+    email: "emccrorie3@netscape.com",
+    department: "Legal",
   },
   {
-    "firstName": "Lorie",
-    "lastName": "Handsheart",
-    "email": "lhandsheart4@fotki.com",
-    "department": "Research and Development"
-  }
+    firstName: "Lorie",
+    lastName: "Handsheart",
+    email: "lhandsheart4@fotki.com",
+    department: "Research and Development",
+  },
 ];
 // Do not edit the code above.
 
@@ -54,19 +54,14 @@ var employees = [
 
 function employeeUpdater() {
   for (let i = 0; i < employees.length; i++) {
-    if (employees[i].firstName === 'Theo') {
-    delete employees[i] 
-  }  else if (employees[i].firstName === 'Lorie') {
-   employees[i].department = 'HR'
-      }
+    if (employees[i].firstName === "Theo") {
+      delete employees[i];
+    } else if (employees[i].firstName === "Lorie") {
+      employees[i].department = "HR";
     }
-    return employees
   }
-
-
-
-
-
+  return employees;
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -106,24 +101,22 @@ function removeDuplicates() {
   return workplaceAccidents;
 }
 
-
-
 ////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
 var cat = {
-  name: 'Fluffy',
+  name: "Fluffy",
   catFriends: [
     {
-      name: 'Grumpy',
-      activities: ['be grumpy', 'eat food']
-    }, 
+      name: "Grumpy",
+      activities: ["be grumpy", "eat food"],
+    },
     {
-      name: 'Lazy Bones',
-      activities: ['sleep', 'pre-sleep naps']
-    }
-  ]
-}
+      name: "Lazy Bones",
+      activities: ["sleep", "pre-sleep naps"],
+    },
+  ],
+};
 // Do not edit the code above.
 
 /*
@@ -136,41 +129,37 @@ var cat = {
 var grumpyActivity;
 var fluffy2ndFriend;
 
-grumpyActivity = cat['catFriends'][0].activities[1]
-console.log(grumpyActivity)
+grumpyActivity = cat["catFriends"][0].activities[1];
+console.log(grumpyActivity);
 
-fluffy2ndFriend = cat['catFriends'][1].name
-console.log(fluffy2ndFriend)
-
-
-
-
+fluffy2ndFriend = cat["catFriends"][1].name;
+console.log(fluffy2ndFriend);
 
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
 var myCar = {
-  make: 'Toyota',
-  model: 'Corolla',
+  make: "Toyota",
+  model: "Corolla",
   year: 1992,
   accidents: [
     {
-      date: '3/15/93',
-      damage: '$5,000',
-      atFaultForAccident: true
+      date: "3/15/93",
+      damage: "$5,000",
+      atFaultForAccident: true,
     },
     {
-      date: '7/4/98',
-      damage: '$2,200',
-      atFaultForAccident: true
+      date: "7/4/98",
+      damage: "$2,200",
+      atFaultForAccident: true,
     },
     {
-      date: '6/22/99',
-      damage: '$7,900',
-      atFaultForAccident: true
-    }
-  ]
-}
+      date: "6/22/99",
+      damage: "$7,900",
+      atFaultForAccident: true,
+    },
+  ],
+};
 // Do not edit the code above.
 
 /*
@@ -184,13 +173,22 @@ var myCar = {
 
 //Code Here
 
-
-
+function recordCleaner() {
+  for (let i = 0; i < myCar['accidents'].length; i++) {
+      if (myCar['accidents'][i]['atFaultForAccident'] === true) {
+        myCar['accidents'][i]["atFaultForAccident"] = false;
+    }
+  }
+}
 
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
-var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
+var numsArr = [
+  [1, 2, 3, 4],
+  [5, 6],
+  [7, 8, 9, 10, 11],
+];
 // Do not edit the code above.
 
 /*
@@ -204,4 +202,16 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
 
 //Code Here
 
-
+function looper() {
+  for (let i = 0; i < numsArr.length; i++) {
+    for (let j = 0; j < numsArr[i].length; j++) {
+      console.log(numsArr,numsArr[i])
+      if (numsArr[i][j] % 2 === 0) {
+        numsArr[i][j] = "even";
+      } else {
+        numsArr[i][j] = "odd";
+      }
+    }
+  }
+  return numsArr;
+}
